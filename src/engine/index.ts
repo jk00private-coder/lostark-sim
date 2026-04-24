1f941478d05556c5dc03ce56097007e0e4395ae3
gbJang <jk00private@gmail.com>
2026-04-24 15:17:43 +0900

    refactor: 구파일 삭제

--- src/engine/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @/engine/index.ts
- * 데미지 엔진 외부 진입점
- */
-
-export * from './atk-calculator';
-export * from './damage-calculator';
-export * from './skill-resolver';
-export * from './skill-damage-runner';
